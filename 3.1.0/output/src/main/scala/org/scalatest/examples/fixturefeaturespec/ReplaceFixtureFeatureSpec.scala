b708f686a8a63624a6df69d39aa3d93db9adc1c0
bvenners <bill@artima.com>
2019-07-27 15:57:23 -0700

    Added tests for fixture.{FeatureSpec, Freespec, FlatSpec, FunSpec, FunSuite, WordSpec}.

---
+++ 3.1.0/output/src/main/scala/org/scalatest/examples/fixturefeaturespec/ReplaceFixtureFeatureSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.examples.fixturefeaturespec
+
+import org.scalatest.fixture
+import java.io._
+import org.scalatest.featurespec
+
+class ReplaceFixtureFeatureSpec extends featurespec.FixtureAnyFeatureSpec {
+
+  case class FixtureParam(file: File, writer: FileWriter)
+
+  def withFixture(test: OneArgTest) = {
+
+    // create the fixture
+    val file = File.createTempFile("hello", "world")
+    val writer = new FileWriter(file)
+    val theFixture = FixtureParam(file, writer)
+
+    try {
+      writer.write("ScalaTest is designed to be ") // set up the fixture
+      withFixture(test.toNoArgTest(theFixture)) // "loan" the fixture to the test
+    }
+    finally writer.close() // clean up the fixture
+  }
+
+  feature("Simplicity") {
+    scenario("User needs to read test code written by others") { f =>
+      f.writer.write("encourage clear code!")
+      f.writer.flush()
+      assert(f.file.length === 49)
+    }
+
+    scenario("User needs to understand what the tests are doing") { f =>
+      f.writer.write("be easy to reason about!")
+      f.writer.flush()
+      assert(f.file.length === 52)
+    }
+  } 
+}
