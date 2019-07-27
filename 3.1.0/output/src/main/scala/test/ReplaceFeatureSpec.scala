50d86efbd1f1c9fe0089a15d6dc4f5207fab86f5
bvenners <bill@artima.com>
2019-07-26 22:40:11 -0700

    Fleshed out style class tests.

---
+++ 3.1.0/output/src/main/scala/test/ReplaceFeatureSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test
+
+import org.scalatest._
+import org.scalatest.featurespec.AnyFeatureSpec
+
+class TVSet {
+  private var on: Boolean = false
+  def isOn: Boolean = on
+  def pressPowerButton() {
+    on = !on
+  }
+}
+
+class ReplaceFeatureSpec extends AnyFeatureSpec with GivenWhenThen {
+
+  info("As a TV set owner")
+  info("I want to be able to turn the TV on and off")
+  info("So I can watch TV when I want")
+  info("And save energy when I'm not watching TV")
+
+  feature("TV power button") {
+    scenario("User presses power button when TV is off") {
+
+      Given("a TV that is switched off")
+      val tv = new TVSet
+      assert(!tv.isOn)
+
+      When("the power button is pressed")
+      tv.pressPowerButton()
+
+      Then("the TV should switch on")
+      assert(tv.isOn)
+    }
+    
+    scenario("User presses power button when TV is on") {
+
+      Given("a TV that is switched on")
+      val tv = new TVSet
+      tv.pressPowerButton()
+      assert(tv.isOn)
+
+      When("the power button is pressed")
+      tv.pressPowerButton()
+
+      Then("the TV should switch off")
+      assert(!tv.isOn)
+    }
+  }
+}
