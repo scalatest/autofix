fcf1d9fbfd5836b8db04c3e057d19def886e1032
bvenners <bill@artima.com>
2019-07-27 15:17:12 -0700

    Fixed package names in diagram test.

--- 3.1.0/input/src/main/scala/org/scalatest/examples/diagrams/ReplaceDiagrammedAssertions.scala
+++ 3.1.0/input/src/main/scala/org/scalatest/examples/diagrams/ReplaceDiagrammedAssertions.scala
@@ -16,7 +16,7 @@ rule = RewriteDeprecatedNames
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest.examples.funsuite
+package org.scalatest.examples.diagrams
 
 import org.scalatest.FunSuite
 import org.scalatest.DiagrammedAssertions
--- 3.1.0/output/src/main/scala/org/scalatest/examples/diagrams/ReplaceDiagrammedAssertions.scala
+++ 3.1.0/output/src/main/scala/org/scalatest/examples/diagrams/ReplaceDiagrammedAssertions.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest.examples.funsuite
+package org.scalatest.examples.diagrams
 
 import org.scalatest.diagrams.Diagrams
 import org.scalatest.funsuite.AnyFunSuite
