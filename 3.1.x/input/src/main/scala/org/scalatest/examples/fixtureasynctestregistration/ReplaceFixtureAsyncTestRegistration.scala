8aa83b5fb66a1c32f48aae707283d65832c3b265
bvenners <bill@artima.com>
2019-09-14 16:54:37 -0700

    Add test for fixture.AsyncTestRegistration => FixtureAsyncTestRegistration rewrite.

---
+++ 3.1.x/input/src/main/scala/org/scalatest/examples/fixtureasynctestregistration/ReplaceFixtureAsyncTestRegistration.scala
@@ -0,0 +1,31 @@
+/*
+rule = RewriteDeprecatedNames
+ */
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.examples.fixturetestregistration
+
+import org.scalatest.fixture
+import scala.concurrent.Future
+import org.scalatest.Assertions.succeed
+
+trait ReplaceFixtureAsyncTestRegistration {
+  def example(tr: fixture.AsyncTestRegistration { type FixtureParam = String }): Unit = {
+    tr.registerAsyncTest("test name")((s: String) => Future.successful(succeed))
+  }
+}
+
+
